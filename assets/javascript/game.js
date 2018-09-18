b073b34c773c6055d4845ec1d954ccc5a8e0bfc8
KI <iqbal.mohammadk@gmail.com>
2018-09-18 14:34:05 -0400

    setting up js

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//create variables for the game and set them equal to 0
+
+var crystal1 = 0;
+var crystal2 = 0;
+var crystal3 = 0;
+var crystal4 = 0;
+
+//make an array for the numbers
+var numbers1 = [Math.floor(Math.random() * Math.floor(900))];
+var numbers2 = [Math.floor(Math.random() * Math.floor(900))];
+var numbers3 = [Math.floor(Math.random() * Math.floor(900))];
+var numbers4 = [Math.floor(Math.random() * Math.floor(900))];
+
