3143e825299bfca6c24cf3a839ec4a0710d8689f
KI <iqbal.mohammadk@gmail.com>
2018-09-19 21:10:10 -0400

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,7 +11,10 @@ var crystal4 = 0;
 //total number of wins and losses
 
 var totalWins = 0;
+$("#wins").text(totalWins);
+
 var totalLosses = 0;
+$("#losses").text(totalLosses);
 
 //total score for current game
 var currentScore = 0;
@@ -19,13 +22,16 @@ var currentScore = 0;
 
 //make an array for the numbers
 
-var numbers1 = Math.floor(Math.random() * (9 - 1 + 1)) + 1; 
+var numbers1 = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
 var numbers2 = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
 var numbers3 = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
 var numbers4 = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
 
 
 console.log("this is number1: " + numbers1)
+console.log("this is number2: " + numbers2)
+console.log("this is number3: " + numbers3)
+console.log("this is number4: " + numbers4)
 
 //variable for computer guess
 var computerGuess = Math.floor(Math.random() * 20) + 50;
@@ -34,47 +40,55 @@ console.log(computerGuess);
 $("#computerGuess").text(computerGuess);
 
 //create event
-$("#image1").on("click", function() {
+$("#image1").on("click", function () {
   console.log("image 1 clicked")
   currentScore = currentScore + numbers1;
+  $("#userScore").text(currentScore);
+});
+
+$("#image2").on("click", function () {
+  console.log("image 2 clicked")
+  currentScore = currentScore + numbers2;
+  $("#userScore").text(currentScore);
+});
+
+$("#image3").on("click", function () {
+  console.log("image 3 clicked")
+  currentScore = currentScore + numbers3;
+  $("#userScore").text(currentScore);
+});
+
+$("#image4").on("click", function () {
+  console.log("image 4 clicked")
+  currentScore = currentScore + numbers4;
+  $("#userScore").text(currentScore);
   console.log(currentScore);
+});
 
-  $("#image2").on("click", function() {
-    console.log("image 1 clicked")
-    currentScore = currentScore + numbers1;
-    console.log(currentScore);
+//variable for user guess
 
-    $("#image3").on("click", function () {
-      console.log("image 1 clicked")
-      currentScore = currentScore + numbers1;
-      console.log(currentScore);
+var userScore = currentScore;
 
-      $("#image4").on("click", function () {
-        console.log("image 1 clicked")
-        currentScore = currentScore + numbers1;
-        console.log(currentScore);
-        // put in 0n screen
+$("#userScore").text(currentScore);
+console.log("loaded");
 
-        // if crr
+//reset the game
+$("#newGame").on("click", function () {
+  totalLosses = 0;
+  totalWins = 0;
+  computerGuess = Math.floor(Math.random() * 20) + 50;
 
-      });
+  console.log(newGameButton);
+});
 
-      //variable for user guess
-      var userGuess;
+//rules
 
-      //reset the game
-      $("#newGame").on("click", function () {
-        totalLosses = 0;
-        totalWins = 0;
-        computerGuess = Math.floor(Math.random() * 20) + 50;
+if (computerGuess === currentScore) {
+  totalWins++;
+  alert("You win!")
+} else {
+  alert("Sorry Sport.")
+  totalLosses++;
 
-        console.log(newGameButton);
-      });
-
-      //rules
-
-      if (computerGuess === userGuess) {
-        totalWins++;
-        } else {
-          totalLosses++;
-      };
+  console.log("alerts")
+};
