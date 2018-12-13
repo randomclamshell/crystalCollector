3a809a9367872126b2405fed6c997de277e39fba
KI <iqbal.mohammadk@gmail.com>
2018-12-13 10:13:01 -0500

    new layout, fixed the reset game bug

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,17 +8,10 @@ var crystal2 = 0;
 var crystal3 = 0;
 var crystal4 = 0;
 
-//total number of wins and losses
-
-var totalWins = 0;
-$("#wins").text(totalWins);
-
-var totalLosses = 0;
-$("#losses").text(totalLosses);
-
 //total score for current game
 var currentScore = 0;
 
+var userScore = currentScore;
 
 //make an array for the numbers
 
@@ -28,10 +21,7 @@ var numbers3 = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
 var numbers4 = Math.floor(Math.random() * (9 - 1 + 1)) + 1;
 
 
-console.log("this is number1: " + numbers1)
-console.log("this is number2: " + numbers2)
-console.log("this is number3: " + numbers3)
-console.log("this is number4: " + numbers4)
+
 
 //variable for computer guess
 var computerGuess = Math.floor(Math.random() * 20) + 50;
@@ -39,23 +29,61 @@ console.log(computerGuess);
 
 $("#computerGuess").text(computerGuess);
 
+function reset() {
+  userScore = 0;
+  computerGuess = Math.floor(Math.random() * 20) + 50;
+  $("#userScore").text(userScore);
+
+console.log("new number1: " + numbers1)
+console.log("new number2: " + numbers2)
+console.log("new number3: " + numbers3)
+console.log("new number4: " + numbers4)
+  } 
+
+function youWon() {
+  alert("You win!");
+  reset();
+}
+
+function youLost() {
+  alert("You lose! Try again!");
+  reset();
+}
+
 //create event
 $("#image1").on("click", function () {
   console.log("image 1 clicked")
   currentScore = currentScore + numbers1;
   $("#userScore").text(currentScore);
+
+  if (currentScore === computerGuess) {
+    youWon();
+  } else if (currentScore > computerGuess) {
+    youLost();
+  }
 });
+
 
 $("#image2").on("click", function () {
   console.log("image 2 clicked")
   currentScore = currentScore + numbers2;
   $("#userScore").text(currentScore);
+  if (currentScore === computerGuess) {
+    youWon();
+  } else if (currentScore > computerGuess) {
+    youLost();
+  }
 });
 
 $("#image3").on("click", function () {
   console.log("image 3 clicked")
   currentScore = currentScore + numbers3;
   $("#userScore").text(currentScore);
+  if (currentScore === computerGuess) {
+    youWon();
+  } else if (currentScore > computerGuess) {
+    youLost();
+  }
 });
 
 $("#image4").on("click", function () {
@@ -63,28 +91,27 @@ $("#image4").on("click", function () {
   currentScore = currentScore + numbers4;
   $("#userScore").text(currentScore);
   console.log(currentScore);
+  if (currentScore === computerGuess) {
+    youWon();
+  } else if (currentScore > computerGuess) {
+    youLost();
+  }
 });
 
 //variable for user guess
 
-var userScore = currentScore;
+
 
 $("#userScore").text(currentScore);
 console.log("loaded");
 
 //reset the game
-$("#newGame").on("click", function () {
-  totalLosses = 0;
-  totalWins = 0;
-  computerGuess = Math.floor(Math.random() * 20) + 50;
-
-  console.log("button click");
+$("button").on("click", function () {
+  reset();
+  console.log(computerGuess);
+  $("#computerGuess").text(computerGuess);
 });
 
-//rules
 
-if (computerGuess === currentScore) {
-  totalWins++;
-} else {
-  totalLosses++;
-};
+
+
